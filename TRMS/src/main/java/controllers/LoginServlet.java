25b7d892a2250fc71c04699bbe97fff7f92be267
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2017-12-26 15:53:03 -0500

    Funky servvlets

--- TRMS/src/main/java/controllers/LoginServlet.java
+++ TRMS/src/main/java/controllers/LoginServlet.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet
  */
-public class Servlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Servlet() {
+    public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,8 +34,20 @@ public class Servlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("passWord");
+		System.out.println("Your login: " +userName + password);
+		
+		PrintWriter writer = response.getWriter();
+		
+		// build HTML code
+		String htmlRespone = "<html>";
+		htmlRespone += "<h2>Your username is: " + userName + "<br/>";		
+		htmlRespone += "Your password is: " + password + "</h2>";		
+		htmlRespone += "</html>";
+		
+		// return response
+		writer.println(htmlRespone);
 	}
 
 }

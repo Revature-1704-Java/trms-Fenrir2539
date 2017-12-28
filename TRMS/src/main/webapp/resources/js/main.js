adaa58fee2dcc4a021497e9f2aa0d886266c6b82
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2017-12-28 16:04:59 -0500

    killing case issue folders

--- TRMS/src/main/webapp/resources/js/main.js
+++
@@ -1,8 +0,0 @@
-const r = new Router (
-    {
-      '#default': new Page('login.html'),
-      home: new Layout(new Page('menu.html'), new Page('login.html')),
-      about: new Layout(new Page('menu.html'), new Page('login.html')),
-    },
-    document.querySelector('main')
-  );

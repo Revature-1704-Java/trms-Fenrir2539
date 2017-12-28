2506e1d63e53b8654d6e43dbbdae5a2a02a4f3bd
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2017-12-28 15:44:40 -0500

    login

--- TRMS/src/main/webapp/resources/js/main.js
+++ TRMS/src/main/webapp/resources/js/main.js
@@ -1,8 +1,8 @@
 const r = new Router (
     {
-      '#default': new Page('menu.html'),
-      home: new Layout(new Page('menu.html'), new Page('home.html')),
-      about: new Layout(new Page('menu.html'), new Page('about.html')),
+      '#default': new Page('login.html'),
+      home: new Layout(new Page('menu.html'), new Page('login.html')),
+      about: new Layout(new Page('menu.html'), new Page('login.html')),
     },
     document.querySelector('main')
   );

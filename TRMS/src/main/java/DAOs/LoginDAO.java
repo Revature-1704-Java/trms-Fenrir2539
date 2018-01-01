4a833fdffbba92d478f7c48648318ad1517926a2
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-01-01 13:46:44 -0500

    Connection work and singletons

--- TRMS/src/main/java/DAOs/LoginDAO.java
+++ TRMS/src/main/java/DAOs/LoginDAO.java
@@ -1,18 +1,46 @@
 package DAOs;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
+
+import Utility.ConnectionSingleton;
 
 public class LoginDAO {
 	
 	/**
-	 * Retrieves a map of usernames and passwords
+	 * Retrieves a map of user names and passwords
 	 * user name is the key to the value password
 	 * 
 	 * 
 	 * @return
 	 */
-	public HashMap<String,String> getUserandPassword() {
-		HashMap userPassMap = new HashMap<String, String>();
+	public HashMap<String, Integer> getUserandPassword() {
+		HashMap<String, Integer> userPassMap = new HashMap<String, Integer>();
+		ResultSet result;
+		
+		//Creation of the statement instance 
+		ConnectionSingleton single = ConnectionSingleton.getInstance();
+		Statement state = ConnectionSingleton.getStatement();
+		// put your SQL code in the variable sqlString
+		String sqlString = "SELECT * FROM LOGIN" ;
+		try {
+			result = state.executeQuery(sqlString);
+			while (result.next()) {
+				int id = result.getInt("EMPLOYEEID");
+				String userName =  result.getString("USERNAME");
+				String pass =  result.getString("PWORD");
+				
+				userPassMap.put(userName+pass, id);
+			}
+			result.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
 		
 		return userPassMap;
 	}
---
+++ TRMS/src/main/java/Utility/ConnectionSingleton.java
@@ -0,0 +1,75 @@
+package Utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class ConnectionSingleton {
+	    private static String db_url;
+	    private static String db_user;
+	    private static String db_password;
+	    private static Statement connection;
+
+	    private ConnectionSingleton() {
+	    	Properties prop = new Properties(); 
+			InputStream in = null;
+			try {
+				in = new FileInputStream("src/main/resources/connection.properties");
+			} catch (FileNotFoundException e) {
+				Logger.getLogger(ConnectionSingleton.class.getName()).log(Level.FATAL, "File does not exist", e);
+			}
+			try {
+				prop.load(in);
+			} catch (IOException e) {
+				Logger.getLogger(ConnectionSingleton.class.getName()).log(Level.FATAL, "Failed to load properties", e);
+			}
+	    	db_url = prop.getProperty("url");
+	        db_user = prop.getProperty("user");
+	        db_password = prop.getProperty("password");
+	        connection = setConnection();
+	    }
+	/* Private method charge to set the connection statement*/
+	    private static Statement setConnection() {
+	        try {
+	            String url = "" + db_url;
+	            java.sql.Connection conn = DriverManager.getConnection(url, db_user, db_password);
+
+	            //Creation of the Statement object
+	            java.sql.Statement state = conn.createStatement();
+	            return (Statement) state;
+	        } catch (SQLException ex) {
+	            Logger.getLogger(ConnectionSingleton.class.getName()).log(Level.FATAL, "Failed to set connection: "+db_url, ex);
+	        }
+	        return null;
+	    }
+
+	/* Private inner class responsible for instantiating the single instance of the singleton */
+	    private static class ConnectionSingletonManagerHolder {
+	        private final static ConnectionSingleton instance = new ConnectionSingleton();
+	    }
+
+	    /**
+	     * @return
+		 *	Public method, which is the only method allowed to return an instance of 
+		 *	the singleton (the instance here is the database connection statement)
+	     */
+	    public static ConnectionSingleton getInstance() {
+	        try {
+	            return ConnectionSingletonManagerHolder.instance;
+	        } catch (ExceptionInInitializerError ex) {
+	        	Logger.getLogger(ConnectionSingleton.class.getName()).log(Level.FATAL, "Failed to get Instance", ex);
+	        }
+	        return null;
+	    }
+	    public static Statement getStatement() {
+	        return connection;
+	    }
+}
